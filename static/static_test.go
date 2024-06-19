277f02989c8f64dafdea6b8964d4a811274e71df
Steve Kemp <steve@steve.org.uk>
2024-06-19 14:44:28 +0300

    Added trivial test

---
+++ static/static_test.go
@@ -0,0 +1,24 @@
+package static
+
+import (
+	"strings"
+	"testing"
+)
+
+// TestStatic just ensures we have some files.
+func TestStatic(t *testing.T) {
+
+	// Read the subdirectory
+	files, err := Content.ReadDir("A")
+	if err != nil {
+		t.Fatalf("error reading contents")
+	}
+
+	// Ensure each file is a .COM files
+	for _, entry := range files {
+		name := entry.Name()
+		if !strings.HasSuffix(name, ".COM") {
+			t.Fatalf("file '%s' is not a .COM file", name)
+		}
+	}
+}
