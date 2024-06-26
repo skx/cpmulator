70fa916977155751612679afa0a2cb570fc336ce
Steve Kemp <steve@steve.org.uk>
2024-06-26 14:23:47 +0300

    More test cases added.

--- cpm/cpm_test.go
+++ cpm/cpm_test.go
@@ -133,3 +133,111 @@ func TestPrinterOutput(t *testing.T) {
 		t.Fatalf("printer output had the wrong content")
 	}
 }
+
+// TestLogNoisy tests that functions are updated appropriately.
+func TestLogNoisy(t *testing.T) {
+
+	obj, err := New("prn", "null", "ccp")
+	if err != nil {
+		t.Fatalf("failed to create CP/M object")
+	}
+
+	// Count the syscalls that are noisy
+	count := func() int {
+		noisy := 0
+
+		for _, syscall := range obj.BIOSSyscalls {
+			if syscall.Noisy {
+				noisy++
+			}
+		}
+		for _, syscall := range obj.BDOSSyscalls {
+			if syscall.Noisy {
+				noisy++
+			}
+		}
+		return noisy
+	}
+
+	// Count before
+	before := count()
+	if before <= 0 {
+		t.Fatalf("count of noisy functions was bogus")
+	}
+
+	// Change the logging
+	obj.LogNoisy()
+
+	// Count after
+	after := count()
+	if after != 0 {
+		t.Fatalf("count of noisy functions was bogus")
+	}
+}
+
+func TestDrives(t *testing.T) {
+
+	obj, err := New("prn", "null", "ccp")
+	if err != nil {
+		t.Fatalf("failed to create CP/M object")
+	}
+
+	// All drives will be "."
+	obj.SetDrives(false)
+
+	// Confirm it
+	for _, x := range obj.drives {
+		if x != "." {
+			t.Fatalf("drive path wrong")
+		}
+	}
+
+	// All drives will be the letter
+	obj.SetDrives(true)
+
+	// Confirm it
+	for k, x := range obj.drives {
+		if x != k {
+			t.Fatalf("drive path wrong")
+		}
+	}
+
+	// Bonus
+	if obj.drives["A"] != "A" {
+		t.Fatalf("bogus A:")
+	}
+	obj.SetDrivePath("A", "STEVE")
+	if obj.drives["A"] != "STEVE" {
+		t.Fatalf("bogus A:")
+	}
+}
+
+// TestCoverage is just coverage messup
+func TestCoverage(t *testing.T) {
+
+	obj, err := New("prn", "null", "ccp")
+	if err != nil {
+		t.Fatalf("failed to create CP/M object")
+	}
+
+	obj.RunAutoExec()
+	obj.In(0x12)
+	obj.Out(0x12, 0x34)
+
+	// Valid: COLDBOOT
+	obj.Out(0xFF, 0x00)
+	if obj.biosErr != nil {
+		t.Fatalf("unexpected error")
+	}
+	// Valid: WARMBOOT
+	obj.Out(0xFF, 0x01)
+	if obj.biosErr != nil {
+		t.Fatalf("unexpected error")
+	}
+
+	// Invalid
+	obj.Out(0xFF, 0xff)
+	if obj.biosErr != ErrUnimplemented {
+		t.Fatalf("expected unimplemented, got %s", obj.biosErr)
+	}
+}
