ec06faad3022c9db82afd09eae2f13ac15db9fa2
Steve Kemp <steve@steve.org.uk>
2025-01-25 09:04:08 +0200

    Cope with fake newlines.   Sigh

--- consolein/drv_file.go
+++ consolein/drv_file.go
@@ -33,13 +33,21 @@ type FileInput struct {
 
 	// content contains the content of the "input.txt" file
 	content []byte
+
+	// fakeNewlines is used to control if we should use
+	// an extra character alongside newlines.
+	fakeNewlines bool
+
+	// inNewline returns true if we're in the middle of a newline
+	// and we need to inject a fake character.
+	inNewline bool
 }
 
-// Setup reads the file "input.txt" and saves the input away for
-// future returning when polled.
+// Setup reads the contents of the file specified by the
+// environmental variable $INPUT_FILE, and saves it away as
+// a source of fake console input.
 //
-// If the file "input.txt" does not exist in the PWD then an error
-// will be returned.
+// If no filename is chosen "input.txt" will be used as a default.
 func (fi *FileInput) Setup() error {
 
 	fileName := os.Getenv("INPUT_FILE")
@@ -52,8 +60,15 @@ func (fi *FileInput) Setup() error {
 		return err
 	}
 
+	// Do we fake newline inputs?  If so set that up now
+	if os.Getenv("INPUT_FAKE_NEWLINES") == "1" {
+		fi.fakeNewlines = true
+	}
+
+	// Save our offset and data.
 	fi.offset = 0
 	fi.content = dat
+
 	return nil
 }
 
@@ -66,9 +81,7 @@ func (fi *FileInput) TearDown() error {
 // can return.  This is always true unless we've exhausted the contents
 // of our input-file.
 func (fi *FileInput) PendingInput() bool {
-
-	// This is designed to ensure that we're not too responsive.
-	time.Sleep(5 * time.Millisecond)
+	time.Sleep(15 * time.Millisecond)
 
 	// If our position is less than the size of the data then
 	// we have data to read, so it is pending.
@@ -80,15 +93,30 @@ func (fi *FileInput) PendingInput() bool {
 func (fi *FileInput) BlockForCharacterNoEcho() (byte, error) {
 
 	// This is designed to ensure that we're not too responsive.
-	time.Sleep(5 * time.Millisecond)
+	time.Sleep(15 * time.Millisecond)
 
+	// If we have input available
 	if fi.offset < len(fi.content) {
+
+		// Get the next character, and move past it.
 		x := fi.content[fi.offset]
 		fi.offset++
 
+		// If we have to deal with \r\n instead
+		// of just \n then deal with that.
+		if fi.inNewline {
+			fi.inNewline = false
+			return '', nil
+		}
+		if x == '\n' && fi.fakeNewlines {
+			fi.inNewline = true
+		}
+
+		// Also allow a sleep to happen.  Sigh.
 		if x == '#' {
 			time.Sleep(1 * time.Second)
 		}
+
 		return x, nil
 	}
 
