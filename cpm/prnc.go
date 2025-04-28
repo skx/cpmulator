c57c5d1ec9af671373a24632846f8212f3de5754
Steve Kemp <steve@steve.org.uk>
2025-04-28 22:06:16 +0300

    Added 100% test-coverage to the prnC method
    This method is used for our printer-logging, and now we have
    100% test-coverage via a mocked os.File interface.

--- cpm/prnc.go
+++ cpm/prnc.go
@@ -5,14 +5,34 @@ import (
 	"os"
 )
 
+// File is an interface that we mock so that we can fake failures to write and close
+// our printer logfile.
+type File interface {
+	// Write is designed to write data, but it does not.
+	// It will either do nothing, or return an error under testing.
+	Write([]byte) (int, error)
+
+	// Close is designed to close a file, but it does not.
+	// It will either do nothing, or return an error under testing.
+	Close() error
+}
+
+// opener is the factory that will allow creating either a real os.File, or a mockFile
+var opener func(name string, flag int, perm os.FileMode) (File, error)
+
 // prnC attempts to write the character specified to the "printer".
 //
 // We redirect printing to use a file, which defaults to "print.log", but
 // which can be changed via the CLI argument
 func (cpm *CPM) prnC(char uint8) error {
 
+	if opener == nil {
+		opener = func(name string, flag int, perm os.FileMode) (File, error) {
+			return os.OpenFile(name, flag, perm)
+		}
+	}
 	// If the file doesn't exist, create it.
-	f, err := os.OpenFile(cpm.prnPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f, err := opener(cpm.prnPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return fmt.Errorf("prnC: Failed to open file %s:%s", cpm.prnPath, err)
 	}
