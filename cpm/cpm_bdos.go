bec81d078ac876296efa5ad40adf4e224ee9e81e
Steve Kemp <steve@steve.org.uk>
2026-08-16 11:32:49 +0300

    Correct the sanity-check which was wrong

--- cpm/cpm_bdos.go
+++ cpm/cpm_bdos.go
@@ -1910,12 +1910,17 @@ func BdosSysCallRandRecord(cpm *CPM) error {
 	offset := fcbPtr.GetSequentialOffset()
 
 	// Now we set the "random record" which is R0,R1,R2
-	fcbPtr.SetRandomOffset(int64(offset) / 128)
+	records := int64(offset) / 128
+	fcbPtr.SetRandomOffset(records)
 
-	// sanity check because I've messed this up in the past
+	// Sanity check
 	n := fcbPtr.GetRandomOffset()
-	if n != offset*128 {
-		return fmt.Errorf("BdosSysCallRandRecord: failed to update because maths is hard %d != %d", n, offset)
+	if n != records {
+		return fmt.Errorf(
+			"BdosSysCallRandRecord: failed to update because maths is hard %d != %d",
+			n,
+			records,
+		)
 	}
 
 	// Update the FCB in memory.
